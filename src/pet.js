75a6ca9500a6f5240d29f51bb9bd1256322f6d6c
SanMasood <sanamasood.giki@gmail.com>
2020-03-18 10:42:49 +0000

    set name property

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,6 @@
-function Pet() {}
-
+function Pet(pname) {
+   this.name = pname;
+   return name;
+ 
+}
 module.exports = Pet;
--- test/pet.test.js
+++ test/pet.test.js
@@ -3,6 +3,12 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(new Pet('Rocky')).toBeInstanceOf(Object);
     });
+
+    it('sets the name property', () => {
+        const pet = new Pet('Rocky');
+    
+        expect(pet.name).toEqual('Rocky');
+      });
   });

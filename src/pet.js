fe25d2a1abacca3364b5f601cf282cd120b44ea3
SanMasood <sanamasood.giki@gmail.com>
2020-03-19 13:09:54 +0000

    Walk method increases fitness

--- src/pet.js
+++ src/pet.js
@@ -1,11 +1,28 @@
+const FITNESS_THRESHOLD = 6;
+
 function Pet(pname) {
    this.name = pname;
    this.age = 0;
+   this.hunger = 0;
+   this.fitness = 10;
    
 
    this.growUp = function(){
        this.age+=1;
+       this.hunger += 5;
+       this.fitness -= 3;
+       
    }
+   this.walk = function (){
+    return (this.fitness >= FITNESS_THRESHOLD) ? this.fitness = 10 : this.fitness+= 4;
+
+}
+   /*this.feedMe = function(){
+       this.hunger -= 1;
+       
+   }*/
+      
+   
  
 }
 module.exports = Pet;
--- test/pet.test.js
+++ test/pet.test.js
@@ -1,28 +1,47 @@
 const Pet = require('../src/pet');
 
-
 describe('constructor', () => {
+  beforeEach(() => {
+    const pet = new Pet('Rocky');
+
+  });
     it('returns an object', () => {
       expect(new Pet('Rocky')).toBeInstanceOf(Object);
     });
 
     it('sets the name property', () => {
-        const pet = new Pet('Rocky');
+       // const pet = new Pet('Rocky');
     
         expect(pet.name).toEqual('Rocky');
       });
 
 
-     it('has a initial age of 0', () => {
-        const pet = new Pet('Rocky');
+     it('has a initial age of 0 & hunger of 0 & fitness of 10', () => {
+       // const pet = new Pet('Rocky');
 
         expect(pet.age).toEqual(0);
+        expect(pet.hunger).toEqual(0);
+        expect(pet.fitness).toEqual(10);
+       
       });
-     it('increments the age by 1', () => {
-        const pet = new Pet('Rocky');
+
+     it('increments the age by 1 & hunger by 5', () => {
+        //const pet = new Pet('Rocky');
 
         pet.growUp();
         expect(pet.age).toEqual(1);
+        expect(pet.hunger).toEqual(5);
+        expect(pet.fitness).toEqual(7);
+  });
+
+  it('walks pet & increments the fitness level by 4 until 10', () => {
+    //const pet = new Pet('Rocky');
+    //fitness is 10 at age 0; fitness is 7 at age 1 & so on
+    pet.growUp(); //fitness: 7
+    pet.walk();
+    
+    expect(pet.fitness).toEqual(10);
+  
   });
   
 });
